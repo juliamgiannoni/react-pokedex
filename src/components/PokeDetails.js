50a25bb801223da8bf56c21d9d585e765ed9c618
Julia Yi <juliamijiyi@gmail.com>
2017-05-30 18:57:33 -0700

    Finish HTML layout

--- src/components/PokeDetails.js
+++ src/components/PokeDetails.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const PokeDetails = () => {
   return (
-    <div>
-    PokeDetails
+    <div className="jumbotron">
+      <h1>Pokemon name goes here</h1>
+      <p>#CampaignHashtag</p>
     </div>
   )
 }
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -3,9 +3,13 @@ import React, {Component} from 'react';
 class PokeList extends Component {
   render() {
     return (
-      <div>
-      PokeList
-      </div>
+      <ul className='list-group'>
+        <li className='list-group-item'>Cras justo odio</li>
+        <li className='list-group-item'>Dapibus ac facilisis in</li>
+        <li className='list-group-item'>Morbi leo risus</li>
+        <li className='list-group-item'>Porta ac consectetur ac</li>
+        <li className='list-group-item'>Vestibulum at eros</li>
+      </ul>
     )
   }
 }
